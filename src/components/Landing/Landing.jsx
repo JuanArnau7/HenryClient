786dad724af4e6ffb6e62f6bf8ec358af4fd27fe
Juan Arnau <juanarnau81@gmail.com>
2022-12-06 15:11:50 -0300

    Añado landing page de Ruben

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -1,8 +1,45 @@
-import React from "react"
+import React from 'react'
+import img from '../Landing/img/restaurant.jpg'
+import img1 from '../Landing/img/ratatouille.jpg'
+import styles from '../Landing/Landing.module.css'
+import { Link } from 'react-router-dom'
+
+
+
+
 const Landing = () => {
-  return(
+  return (
     <>
-      <h1>This is a Landing component</h1>
+      <div className={styles.background}>
+      <div className={styles.img}>
+        <img src={img} alt="LandingPage" />
+      </div>
+
+        <div className={styles.contTile}>
+
+          <div className={styles.CardTitle}>
+            <div className={styles.Title}>Welcome</div>
+            <div className={styles.parrafo}>
+              
+            </div>
+            <Link to="/home">
+              <button className={styles.button}>Let's go !</button>
+            </Link>
+          </div>
+            <div className={styles.Title}> Henry Food Restaurant!</div>
+
+        </div>
+
+        <div className={styles.contGlobo}>
+          <div className={styles.contimg}>
+          <div className={styles.img1}>
+            <img src={img1} alt="LandingPage" />
+          </div>
+
+          </div>
+        </div>
+
+      </div>
     </>
   )
 }
