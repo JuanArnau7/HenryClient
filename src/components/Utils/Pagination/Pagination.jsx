bac97add632c4bb803b0f7b211ac01e807914ebf
Guorvo <lmcuchero@gmail.com>
2022-12-07 19:29:38 -0300

    Added pagination, menu box and basic loading, please check code

--- src/components/Utils/Pagination/Pagination.jsx
+++ src/components/Utils/Pagination/Pagination.jsx
@@ -1,9 +1,21 @@
-const Pagination = () => {
-    return(
-      <>
-        <h1>This is a Pagination component</h1>
-      </>
-    )
+import React from 'react';
+
+// https://github.com/bradtraversy/simple_react_pagination
+const Pagination = ({ postsPerPage, totalPosts, paginate }) => {
+  const pageNumbers = [];
+
+  for (let i = 1; i <= Math.ceil(totalPosts / postsPerPage); i++) {
+    pageNumbers.push(i);
   }
-  
-  export default Pagination
+
+  return (
+    <div>
+      {pageNumbers.map(number => (
+        <button key={`page-${number}`} onClick={() => paginate(number)}>
+          {number}</button>
+      ))}
+    </div>
+  );
+};
+
+export default Pagination;
