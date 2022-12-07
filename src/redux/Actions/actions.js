ef2f4149954b9e9451defb3607dd4c8db886cc07
Juan Arnau <juanarnau81@gmail.com>
2022-12-07 15:50:17 -0300

    Añado Formulario de creacion de Users funcionand (falta validar contraseña y setear errores) agregue action y reducer de Post users

--- src/redux/Actions/actions.js
+++ src/redux/Actions/actions.js
@@ -1,1 +1,13 @@
-import './actionsTypes'
+import { POST_USER_CREATE } from './actionsTypes'
+import axios from 'axios'
+export function postUserCreate(payload) {
+    return async function(dispatch){
+        try {
+            const res = await axios.post(`http://localhost:3001/users`, payload)
+            dispatch({ type: POST_USER_CREATE, payload : res.payload});
+            return res
+        } catch (error) {
+            alert ("Connection to /Post Users Failed. ERROR:"+ error)
+        }
+    }
+}
