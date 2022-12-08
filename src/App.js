6312d64ecaf4535effcc071678ffc3b6cb1ff42a
AlexTerrussi09 <alerterrussi@gmail.com>
2022-12-08 15:23:50 -0300

    actualizado

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import { Route, BrowserRouter, Routes } from 'react-router-dom'
-import Home from './components/Home/Home';
+import LocalHome from './components/Local/Home/Home';
+import DeliveryHome from './components/Delivery/Home/Home';
 import Landing from './components/Landing/Landing';
-import RegisterMenus from './components/RegisterMenus/RegisterMenus';
-import RegisterDishes from './components/RegisterDishes/RegisterDishes';
+import RegisterMenus from './components/Dashboard/RegisterMenus/RegisterMenus';
+import RegisterDishes from './components/Dashboard/RegisterDishes/RegisterDishes';
 import Register from './components/Register/Register'
 import PerfilUsuario from './components/PerfilUsuario/PerfilUsuario';
 
@@ -12,11 +13,11 @@ function App() {
     <BrowserRouter>
     <Routes>
         <Route exact path="/" element={<Landing/>}></Route>
-        <Route exact path="/Home" element={<Home/>} />
-        <Route exact path="/PerfilUsuario" element={<PerfilUsuario/>} />
-        <Route exact path='/RegisterMenus' element={<RegisterMenus/>} />
-        <Route exact path='/RegisterDishes' element={<RegisterDishes/>} />
-        <Route exact path='/Register' element={<Register/>} />
+        <Route exact path="/local/home" element={<LocalHome/>} />
+        <Route exact path="/deliver/home" element={<DeliveryHome/>} />
+        <Route exact path='/dashboard/menus' element={<RegisterMenus/>} />
+        <Route exact path='/dashboard/dishes' element={<RegisterDishes/>} />
+        <Route exact path='/register' element={<Register/>} />
     </Routes>
     </BrowserRouter>
 
