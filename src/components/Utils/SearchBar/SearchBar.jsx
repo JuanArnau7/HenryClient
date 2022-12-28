c6485a4cc9ce459b29bac6eac6b811782d0ac8b0
Gonzzzza <gonzalotrevisani1105@gmail.com>
2022-12-28 14:17:10 -0300

    searchBar

--- src/components/Utils/SearchBar/SearchBar.jsx
+++ src/components/Utils/SearchBar/SearchBar.jsx
@@ -14,6 +14,7 @@ const SearchBar = ({manageMenu}) => {
     filtrar(e.target.value)
   }
 
+  
   return (
     <>
       <button type="button" data-collapse-toggle="navbar-search" aria-controls="navbar-search" aria-expanded="false" className="md:hidden text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5 mr-1" onClick={() => manageMenu()}>
