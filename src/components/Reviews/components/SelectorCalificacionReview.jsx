ba4c7875dfb45357c8bc95f2ec369d7968867b04
Juan Arnau <juanarnau81@gmail.com>
2022-12-29 13:42:00 -0300

    Arreglo de bugs

--- src/components/Reviews/components/SelectorCalificacionReview.jsx
+++ src/components/Reviews/components/SelectorCalificacionReview.jsx
@@ -7,7 +7,7 @@ const SelectorCalificacionReview = ({setCalificacion}) => {
     }
   return (
     <>
-    <label for="countries" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Qualification</label>
+    <label htmlFor="countries" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Qualification</label>
         <select className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" onChange={handleChange}>
         <option value="">Choose a option</option>
         <option value="EXCELENTE">Excellent</option>
