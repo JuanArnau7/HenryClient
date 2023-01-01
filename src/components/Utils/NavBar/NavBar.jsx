81051c023a4a668091f6cb542a413b70f4358ee5
Gonzzzza <gonzalotrevisani1105@gmail.com>
2023-01-01 20:01:03 -0300

    sidebar

--- src/components/Utils/NavBar/NavBar.jsx
+++ src/components/Utils/NavBar/NavBar.jsx
@@ -44,7 +44,7 @@ const NavBar = ({handleLogOut, handleCreate}) => {
   return (
     <>
       <nav className=" px-2 sm:px-4 py-2.5 bg-green-700 shadow-xl  w-full">
-      <Sidebar />
+      {/* <Sidebar /> */}
         <div className="container flex flex-wrap items-center justify-between mx-auto">
           <Link to='/' ><span className="flex items-center">
             <span className="px-2 self-center text-xl text-white font-semibold whitespace-nowrap dark:text-white">Henry's Foods</span>
