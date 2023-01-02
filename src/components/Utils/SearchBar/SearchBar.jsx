99acbab548fb7fec57753be1dea3b8626540a1dd
AlexTerrussi09 <alerterrussi@gmail.com>
2023-01-02 04:10:47 -0300

    sidebar integrada

--- src/components/Utils/SearchBar/SearchBar.jsx
+++ src/components/Utils/SearchBar/SearchBar.jsx
@@ -29,7 +29,7 @@ const SearchBar = ({manageMenu}) => {
         <svg className="w-5 h-5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clipRule="evenodd"></path></svg>
         <span className="sr-only">Search</span>
       </button>
-      <div className="relative hidden md:block w-1/2" >
+      <div className="relative block md:block w-1/2" >
         <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none" onClick={() => manageMenu()}>
           <svg className="w-5 h-5 text-gray-500" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clipRule="evenodd"></path></svg>
           <span className="sr-only">Search icon</span>
