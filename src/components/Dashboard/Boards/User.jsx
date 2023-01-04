cbea45fe8883caaeb28bdd5b376e6ea5a8d31720
Guorvo <lmcuchero@gmail.com>
2023-01-04 17:39:52 -0300

    crappy user table, need functions

--- src/components/Dashboard/Boards/User.jsx
+++ src/components/Dashboard/Boards/User.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import Table from "../../Utils/Table/Table";
 
 const BoardUser = () => {
   return (
     <div>
-      <h1>BoardUser</h1>
+      <Table/>
     </div>
   )
 }
