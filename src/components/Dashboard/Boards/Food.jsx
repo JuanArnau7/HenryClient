7774de704e469ca3ca32d0a245b87a578c94d6db
Juan Arnau <juanarnau81@gmail.com>
2023-01-08 21:24:17 -0300

    CRUD de admin foods funcional, hagan npm i

--- src/components/Dashboard/Boards/Food.jsx
+++ src/components/Dashboard/Boards/Food.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import CRUDTable,
 {
   Fields,
@@ -8,27 +7,41 @@ import CRUDTable,
   UpdateForm,
   DeleteForm,
 } from 'react-crud-table';
-
+import { useDispatch, useSelector } from 'react-redux';
+import { Navigate, redirect, useNavigate } from 'react-router-dom';
+import { deleteFood } from '../../../redux/Actions/actions';
 // Component's Base CSS
-// import './index.css';
-const BoardFoods = () => {
+import './Food.css';
 
+const BoardFoods = () => {
+  
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
+  const dishes = useSelector(state => state.filterDishes)
 const DescriptionRenderer = ({ field }) => <textarea {...field} />;
+console.log('dishes', dishes);
+
+// const mapeo =   dishes.map(d=>({id: d._id,title:d.lenguage.en.name, description:d.lenguage.en.descripcion})) 
+// console.log('mapeo',mapeo);
 
 let tasks = [
-
+  
   // aca deberian ir todos los platos mapeados, traerlos con la action
-  {
-    id: 1,
-    title: 'Create an example',
-    description: 'Create an example of how to use the component',
-  },
-  {
-    id: 2,
-    title: 'Improve',
-    description: 'Improve the component!',
-  },
+  // {
+    //   id: 1,
+    //   title: 'Create an example',
+    //   description: 'Create an example of how to use the component',
+    // },
+  // {
+    //   id: 2,
+  //   title: 'Improve',
+  //   description: 'Improve the component!',
+  // },
+  dishes.map(d=>({id: d._id,title:d.lenguage.en.name, description:d.lenguage.en.descripcion, visible:d.state.toString()})) 
+  // mapeo
 ];
+// console.log('tasks', tasks[0]);
+
 
 const SORTERS = {
   NUMBER_ASCENDING: mapper => (a, b) => mapper(a) - mapper(b),
@@ -53,31 +66,16 @@ const getSorter = (data) => {
 };
 
 
-let count = tasks.length;
+// let count = tasks.length;
+// fetchItems()
 const service = {
   fetchItems: (payload) => {
-    let result = Array.from(tasks);
+    // console.log("PAYLOAD", payload)
+    let result = Array.from(tasks[0]);
+    // console.log('RESULT', result);
+    
     result = result.sort(getSorter(payload.sort));
     return Promise.resolve(result);
-  },
-  create: (task) => {
-    count += 1;
-    tasks.push({
-      ...task,
-      id: count,
-    });
-    return Promise.resolve(task);
-  },
-  update: (data) => {
-    const task = tasks.find(t => t.id === data.id);
-    task.title = data.title;
-    task.description = data.description;
-    return Promise.resolve(task);
-  },
-  delete: (data) => {
-    const task = tasks.find(t => t.id === data.id);
-    tasks = tasks.filter(t => t.id !== task.id);
-    return Promise.resolve(task);
   },
 };
 
@@ -85,13 +83,27 @@ const styles = {
   container: { margin: 'auto', width: 'fit-content' },
 };
 
+const handleSubmit = (e) =>{
+  console.log('EEEE', e.id);
+  // e.preventDefault()
+  dispatch(deleteFood(e.id))
+  window.location.reload()
+  // alert("funciona esta porqueria")
+}
+const handleClickCreate = (e) => {
+  e.preventDefault()
+  navigate('/createFood')
+}
+
 return(
   <>
-
+  <button onClick={handleClickCreate}>create new food</button>
 <div style={styles.container}>
     <CRUDTable
-      caption="Tasks"
+      caption="History"
       fetchItems={payload => service.fetchItems(payload)}
+      items={tasks[0]}
+      // items={result}
     >
       <Fields>
         <Field
@@ -106,31 +118,16 @@ return(
           placeholder="Title"
         />
         <Field
+          name="visible"
+          label="Visible"
+          placeholder="Visible"
+        />
+        <Field
           name="description"
           label="Description"
           render={DescriptionRenderer}
         />
       </Fields>
-      <CreateForm
-        title="Task Creation"
-        message="Create a new task!"
-        trigger="Create Task"
-        onSubmit={task => service.create(task)}
-        submitText="Create"
-        validate={(values) => {
-          const errors = {};
-          if (!values.title) {
-            errors.title = 'Please, provide task\'s title';
-          }
-
-          if (!values.description) {
-            errors.description = 'Please, provide task\'s description';
-          }
-
-          return errors;
-        }}
-      />
-
       <UpdateForm
         title="Task Update Process"
         message="Update task"
@@ -155,12 +152,11 @@ return(
           return errors;
         }}
       />
-
       <DeleteForm
         title="Task Delete Process"
         message="Are you sure you want to delete the task?"
         trigger="Delete"
-        onSubmit={task => service.delete(task)}
+        onSubmit={(e) => handleSubmit(e)}
         submitText="Delete"
         validate={(values) => {
           const errors = {};
@@ -176,5 +172,5 @@ return(
 );
       }
 
-BoardFoods.propTypes = {};
+// BoardFoods.propTypes = {};
 export default BoardFoods
