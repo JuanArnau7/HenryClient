59f39d163384ef0e998ba562652179a077b227ca
Guorvo <lmcuchero@gmail.com>
2023-01-09 12:23:19 -0300

    dashboard delete unnecessary hr

--- src/components/Dashboard/AdminSidebar.jsx
+++ src/components/Dashboard/AdminSidebar.jsx
@@ -159,11 +159,11 @@ const AdminSidebar = ({ sidebar, handlerSidebar }) => {
           </div>
         </nav>
 
-        <header class="bg-white shadow">
+        {/* <header class="bg-white shadow">
           <div class="mx-auto max-w-7xl py-6 px-4 sm:px-6 lg:px-8">
             <h1 class="text-3xl font-bold tracking-tight text-gray-900">Dashboard</h1>
           </div>
-        </header>
+        </header> */}
         {/* <main>
             <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
               <div class="px-4 py-6 sm:px-0">
