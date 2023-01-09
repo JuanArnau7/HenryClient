ad39f0b95cfe47a21bdd414fd72ee5f3f6e67857
JuanArnau7 <98850106+JuanArnau7@users.noreply.github.com>
2023-01-09 18:16:35 -0300

    arreglo error board/tags

--- src/components/Dashboard/Boards/Tag.jsx
+++ src/components/Dashboard/Boards/Tag.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-import Table from "../../Utils/Table/Table";
+// import Table from "../../Utils/Table/Table";
 
 const BoardTag = () => {
   return (
     <div>
       <h1>ESTA ES UNA MALDITA TAGS</h1>
-    <Table/>
+   // <Table/>
   </div>
   )
 }
