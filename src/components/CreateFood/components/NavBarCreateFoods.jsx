734b7f65cc0168e9a95567af509c491bac1d4d08
Eduardo Laredo <eduardo.fco.laredo.mateos@gmail.com>
2023-01-10 13:47:09 -0800

    fomurlario food

--- src/components/CreateFood/components/NavBarCreateFoods.jsx
+++ src/components/CreateFood/components/NavBarCreateFoods.jsx
@@ -4,7 +4,7 @@ const NavBarCreateFoods = props => {
 
   return (
     <>
-    <nav className="bg-green-700 px-2 sm:px-4 py-2.5 fixed">
+    <nav className="bg-green-700 px-2 sm:px-4 py-2.5">
   <div className="mx-auto w-screen px-2 sm:px-6 lg:px-8">
     <div className="relative flex h-16 items-center justify-between">
       <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
